80825ac8cb683ed386feab001d6f8e11841a049b
Prosenjit Manna <prosenjit@itobuz.com>
2019-04-13 00:55:58 +0530

    output setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import image from  './bimage.jpg';
 
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+   var imageObj = new Image();
+   imageObj.src = image;
+
+   element.appendChild(imageObj);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  watch: true,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html'),
+      filename: 'index.html',
+      hash: true
+    })
+  ],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 };
